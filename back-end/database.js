b213431dd5f67d2be84ef0c1b131dd28fb22abd0
yo <20220224@ricaldone.edu.sv>
2025-02-28 15:36:46 -0600

    changes

--- back-end/database.js
+++ back-end/database.js
@@ -11,4 +11,4 @@ const connection = mongoose.connection;
 
 connection.once("open", () => {console.log("db is connected")}) //si se conecta pasa esto
 connection.on("disconnected", () => {console.log("db is disconnected")}) //si no pasa esto
-connection.on("error",() => { console.log("ERROR: You're a fucking nigger")}) //si ripea pasa esto
+connection.on("error",() => { console.log("ERROR")}) //si ripea pasa esto
